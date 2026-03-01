5e00e26dafff3f75b2f914fd71bfd804e6a8b241
Alilacream <amaramohammedali42@gmail.com>
2026-03-01 21:26:58 +0000

    Black Jack mini game

--- Black-Jack/script.js
+++ Black-Jack/script.js
@@ -1,1 +1,66 @@
+let player = {
+  name: "Ali",
+  chips: 40,
+};
+let cards = [];
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = false;
+let message = "";
+// this isn't the best practices of DOM, but we all try :P
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+// 3. Grab ahold of the player-el paragraph and store it in a variable called playerEl
+let playersEl = document.getElementById("players-el");
+// 4. Render the player's name and chips in playerEl
+playersEl.textContent = player.name + ": $" + player.chips;
+// You get a random card between the ACE (1) to the Kings (11 <-> 13).
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
+}
+// i learned that i should make a call function when someone wanna start the game.
+function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+  renderGame();
+}
+// - This displays how many cards.
+// - Logic
+function renderGame() {
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
 
+  sumEl.textContent = "Sum: " + sum;
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
+}
+// - NewCard function that uses the getRandomCard
+function newCard() {
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
+}
