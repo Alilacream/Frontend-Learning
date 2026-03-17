4d43b2fe3a8d46e81643907b9ba3ce38ab6643e1
Alilacream <amaramohammedali42@gmail.com>
2026-03-17 01:27:05 +0000

    Almost Done with the Chrome extension

--- Chrome_Extension/script.js
+++ Chrome_Extension/script.js
@@ -2,17 +2,40 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+// get all elements from the inputs key
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("inputs"));
+
+// load the myleads as the history
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads(myLeads);
+}
+
+// played with a simple parameter
+function renderLeads(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `;
+  }
+  ulEl.innerHTML = listItems;
+}
+
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  renderLeads(myLeads);
+});
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
-  renderLeads();
+  localStorage.setItem("inputs", JSON.stringify(myLeads));
+  renderLeads(myLeads);
 });
-
-function renderLeads() {
-  let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
-    listItems += `<li><a href=${myLeads[i]}>${myLeads[i]}</a></li>`;
-  }
-  ulEl.innerHTML = listItems;
-}
