4280ed6cdc65929d946bc88f4338f1a073c97548
Alilacream <amaramohammedali42@gmail.com>
2026-07-08 00:40:47 +0100

    added Ai functionnality, but the promise is messing up
    Error: Cannot select auto-router when using non-Hugging Face API key.

---
+++ Chef_Claude/ai.js
@@ -0,0 +1,22 @@
+import { InferenceClient } from '@huggingface/inference'
+import { SYSTEM_PROMPT, AI_API_KEY } from './config'
+const hf = new InferenceClient(AI_API_KEY)
+export async function getHfResponse(data) {
+  // first time using try catch method 
+  // NOTE: hand coding is fun :3, until you hit a bug :<
+  try {
+    const result = await hf.chatCompletion({
+      model: "meta-llama/Llama-3.1-8B-Instruct", // or any chat model on HF
+      messages: [
+        { role: "system", content: SYSTEM_PROMPT },
+        { role: "user", content: data }
+      ],
+      max_tokens: 1024
+    })
+    return result.choices[0].message
+  } catch (e) {
+    console.log(e.message)
+  } finally {
+    console.log("Recipe Done :3")
+  }
+}
