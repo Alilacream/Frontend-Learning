2ed1193cb4afe0fcf8cbddfc9ebb63c031fbaeab
Alilacream <amaramohammedali42@gmail.com>
2026-07-15 13:01:44 +0100

    Last challenge done, now leaves the additional functionnality i want

--- Chef_Claude/components/getIngredients.jsx
+++ Chef_Claude/components/getIngredients.jsx
@@ -1,9 +1,11 @@
+import Markdown from "react-markdown"
+// Last challenge completed
 export default function GetIng(props) {
   return (
     <section>
       <h2>Chef Claude Recommends:</h2>
       <article className="suggested-recipe-container" aria-live="polite">
-        <p>{props.response}</p>
+        <Markdown>{props.response}</Markdown>
       </article>
     </section>
   )
