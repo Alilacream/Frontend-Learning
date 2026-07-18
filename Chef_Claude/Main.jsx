75a525d0fa1c729cccec3f5d2e00a608ea96544a
Alilacream <amaramohammedali42@gmail.com>
2026-07-18 22:34:51 +0100

    Scroll into view behavior

--- Chef_Claude/Main.jsx
+++ Chef_Claude/Main.jsx
@@ -1,38 +1,17 @@
-import React from "react"
+import { useState, useEffect, useRef } from "react"
 import Form from "./components/Form"
 import Ingredients from "./components/Ingredients"
 import Recipe from "./components/Recipe"
-import GetIng from "./components/getIngredients"
+import GetIng from "./components/getRecipe"
 import { getHfResponse } from "./ai"
 export default function Main() {
-  /**
-   * Challenge: clean up our code!
-   * Let's make a couple new components to make things a
-   * little cleaner. (Notice: I'm not suggesting what we
-   * have now is bad or wrong. I'm mostly finding an excuse
-   * to get in some hands-on practice 🙂)
-   * 
-   * 1. Move the entire recipe <section> into its own
-   *    ClaudeRecipe component
-   * 2. Move the list of ingredients <section> into its
-   *    own IngredientsList component.
-   * 
-   * While you're considering how to structure things, consider
-   * where state is, think about if it makes sense or not to
-   * move it somewhere else, how you'll communicate between
-   * the parent/child components, etc.
-   * 
-   * The app should function as it currently does when you're
-   * done, so there will likely be some extra work to be done
-   * beyond what I've listed above.
-   * Done <:
-   */
 
-  const [ingredients, setIngredients] = React.useState([])
+
+  const [ingredients, setIngredients] = useState([])
   // changed it to string, to change state into the ai response
-  const [recipe, setRecipe] = React.useState("")
+  const [recipe, setRecipe] = useState("")
 
-
+  const recipeSection = useRef(null)
   // NOTE: you can use await method only in async functions
   async function getRecipe() {
     const recipeMarkdown = await getHfResponse(ingredients)
@@ -44,15 +23,21 @@ export default function Main() {
     const newIngredient = formData.get("ingredient")
     setIngredients(prevIngredients => [...prevIngredients, newIngredient])
   }
-
+  // using useRef
+  useEffect(() => {
+    if (recipe !== "" && recipeSection.current !== null) {
+      recipeSection.current.scrollIntoView()
+    }
+  }, [recipe])
   return (
     <main>
       {/** shuf dir hna forum */}
       <Form addIngredient={addIngredient} />
-      {ingredients.length > 0 && <section>
-        <Ingredients list={ingredients} />
-        {ingredients.length > 3 && <Recipe toggle={getRecipe} />}
-      </section>}
+      {ingredients.length > 0 &&
+        <section>
+          <Ingredients list={ingredients} />
+          {ingredients.length > 3 && <Recipe toggle={getRecipe} ref={recipeSection} />}
+        </section>}
       {recipe && <GetIng response={recipe} />}
     </main>
   )
--- Chef_Claude/components/Recipe.jsx
+++ Chef_Claude/components/Recipe.jsx
@@ -1,11 +1,11 @@
 export default function Recipe(props) {
-    return (
-        <div className="get-recipe-container">
-                    <div>
-                        <h3>Ready for a recipe?</h3>
-                        <p>Generate a recipe from your list of ingredients.</p>
-                    </div>
-                    <button onClick={props.toggle}>Get a recipe</button>
-                </div>
-    )
+  return (
+    <div className="get-recipe-container">
+      <div ref={props.ref}>
+        <h3>Ready for a recipe?</h3>
+        <p>Generate a recipe from your list of ingredients.</p>
+      </div>
+      <button onClick={props.toggle}>Get a recipe</button>
+    </div>
+  )
 }
