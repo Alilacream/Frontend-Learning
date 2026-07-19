39272bb64b1b26c53191e00409f79f9ffd28453c
Alilacream <amaramohammedali42@gmail.com>
2026-07-19 15:47:29 +0100

    Tenziez game Done

--- Tenziez/App.jsx
+++ Tenziez/App.jsx
@@ -3,30 +3,39 @@ import Die from "./Die"
 import { nanoid } from "nanoid"
 
 export default function App() {
-  /**
-   * Challenge: Add conditional styling to the Die component
-   * so that if it's held (isHeld === true), its background color
-   * changes to a light green (#59E391)
-   * 
-   * Remember: currently the Die component has no way of knowing
-   * if it's "held" or not.
-   */
-
+  // state that returns an array of 10 random dices
   const [dice, setDice] = useState(generateAllNewDice())
-
+  // func that'll generate 10 random dices
   function generateAllNewDice() {
     return new Array(10)
       .fill(0)
       .map(() => ({
-        value: Math.ceil(Math.random() * 6),
+        value: randDice(),
         isHeld: false,
         id: nanoid()
       }))
   }
-
-  function rollDice() {
-    setDice(generateAllNewDice())
+  // gives random number for one dice
+  function randDice() {
+    return Math.floor(Math.random() * 7)
   }
+
+  // role all dices
+  function rollDice() {
+    // if the game is won, we can make a new game using the same function
+    if (checkGame) {
+      setDice(generateAllNewDice())
+      return
+    }
+    // role specific dices that have isheld false
+    setDice(prev => {
+      return prev.map(obj => obj.isHeld ?
+        obj :
+        { ...obj, value: randDice() }
+      )
+    })
+  }
+  // A hold function for an onClick event: the number of a certain Dice is locked 
   const hold = (id) => {
     setDice(oldArr => {
       return oldArr.map(element => element.id === id ?
@@ -35,6 +44,7 @@ export default function App() {
       )
     })
   }
+  // dice elements that'll rendre in DOM
   const diceElements = dice.map(dieObj =>
     <Die key={dieObj.id}
       value={dieObj.value}
@@ -42,13 +52,15 @@ export default function App() {
       toggle={hold}
       id={dieObj.id}
     />)
-
+  // check if all dices have the same numbers and they're held
+  const checkGame = dice.every(obj => obj.isHeld === true && obj.value === dice[1].value)
   return (
     <main>
+      {chcek && <h1>Congrats 🥳✅💯</h1>}
       <div className="dice-container">
         {diceElements}
       </div>
-      <button className="roll-dice" onClick={rollDice}>Roll</button>
+      <button className="roll-dice" onClick={rollDice}>{checkGame ? "New Game" : "Roll"}</button>
     </main>
   )
 }
