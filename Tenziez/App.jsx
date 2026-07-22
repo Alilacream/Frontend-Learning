72ee46ecd902c0685edc054dd52e1015685c23eb
Alilacream <amaramohammedali42@gmail.com>
2026-07-22 01:17:13 +0100

    Last challenge of the game is done
    Keyboard auto selecting the new game button once it's over.

--- Tenziez/App.jsx
+++ Tenziez/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react"
+import { useRef, useState, useEffect } from "react"
 import Die from "./Die"
 import { nanoid } from "nanoid"
 
 export default function App() {
+  const quickPress = useRef(null)
+  console.log(quickPress)
   // state that returns an array of 10 random dices
   const [dice, setDice] = useState(generateAllNewDice())
   // func that'll generate 10 random dices
@@ -23,7 +25,7 @@ export default function App() {
   // role all dices
   function rollDice() {
     // if the game is won, we can make a new game using the same function
-    if (checkGame) {
+    if (gameDone) {
       setDice(generateAllNewDice())
       return
     }
@@ -53,14 +55,27 @@ export default function App() {
       id={dieObj.id}
     />)
   // check if all dices have the same numbers and they're held
-  const checkGame = dice.every(obj => obj.isHeld === true && obj.value === dice[1].value)
+  const gameDone = dice.every(obj => obj.isHeld === true && obj.value === dice[1].value)
+  // making the keyboard only users focus the new game button directly without multi tabing
+  useEffect(() => {
+    if (quickPress.current === null) {
+      return
+    }
+    quickPress.current = quickPress.current.focus()
+  }, [gameDone])
+  // checking if the quickPress ref should contain the DOM element of button
+  // if the game is done, then the tabClick will be active once the useEffect is ran
+  const tabClick = gameDone ? quickPress : null
   return (
+
     <main>
-      {checkGame && <h1>Congrats 🥳✅💯</h1>}
+      <h1>{gameDone ? "Try New Game ?" : "Tenziez"}</h1>
+      <p>A game that made me understand a bit how to handle myself with useRef, useState, and useEffect, mohime bs7a l game hak 9cheb shiwiya</p>
+      {gameDone && <h2>Congrats 🥳✅💯</h2>}
       <div className="dice-container">
         {diceElements}
       </div>
-      <button className="roll-dice" onClick={rollDice}>{checkGame ? "New Game" : "Roll"}</button>
+      <button ref={tabClick} className="roll-dice" onClick={rollDice}>{gameDone ? "New Game" : "Roll"}</button>
     </main>
   )
 }
